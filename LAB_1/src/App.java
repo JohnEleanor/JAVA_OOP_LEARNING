7a84159a7b83b9da5a9fc8bca07dfc100b2e66e1
John Eleanor <mrs.ch64@gmail.com>
2023-12-08 20:17:49 +0700

    .

--- LAB_1/src/App.java
+++ LAB_1/src/App.java
@@ -1,10 +1,9 @@
 import javax.swing.JOptionPane;
 
 public class App {
-    String msg;
+    
 
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        msg = JOptionPane.showMessageDialog(null, "HELLO");
+ 
     }
 }
