e0fb7dc1ea37506481ca64aa40a663a81aae7ced
John Eleanor <mrs.ch64@gmail.com>
2024-02-08 22:10:41 +0700

    Update Format String

--- LAB_6/src/TestParcelTerry.java
+++ LAB_6/src/TestParcelTerry.java
@@ -1,9 +1,10 @@
+import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 public class TestParcelTerry {
     public static void main(String[] args) {
 
-
+        DecimalFormat number = new DecimalFormat("#,###,###,###");
         ParcelTerry user1 = new ParcelTerry();
 
         String Weight_input =  JOptionPane.showInputDialog("Plase Enter Your Weight : ");
@@ -18,13 +19,18 @@ public class TestParcelTerry {
 
         JOptionPane.showMessageDialog(null, "This is Price : "+Calculate);
 
-        String paymoney = JOptionPane.showInputDialog("Plase Enter Your Paymoney [ You Need To Pay "+Calculate+" ] : ");
+        String paymoney = JOptionPane.showInputDialog("Plase Enter Your Paymoney [ You Need To Pay "+number.format(Calculate)+" ] : ");
 
         user1.Setpaymoney(Integer.parseInt(paymoney));
         user1.Setweight(weight);
 
+        double PaymoreMoney = Calculate - user1.getPaymoney();
+
+        String formattedPaymoreMoney = number.format(PaymoreMoney);
+
+        
         if (user1.getPaymoney() < Calculate) {
-            JOptionPane.showMessageDialog(null, "You Need To Pay More Money");
+            JOptionPane.showMessageDialog(null, "You Need To Pay More Money "+formattedPaymoreMoney+" Bath");
  
         }else {
 
